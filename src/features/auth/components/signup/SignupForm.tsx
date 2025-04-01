f6e64a7dfe91fb7d1e8edd9aa64694aaea25ca45
phirom-02 <phiromkhim02@gmail.com>
2025-04-01 10:32:38 +0700

    temp

--- src/features/auth/components/signup/SignupForm.tsx
+++ src/features/auth/components/signup/SignupForm.tsx
@@ -1,4 +1,11 @@
-export default function SignupForm() {
-  return <div>SignupForm</div>;
-}
+import { Input } from "@/components/ui/input";
 
+export default function SignupForm() {
+  return (
+    <div>
+      <Input placeholder="username" />
+      <Input placeholder="email" />
+      <Input placeholder="password" />
+    </div>
+  );
+}
