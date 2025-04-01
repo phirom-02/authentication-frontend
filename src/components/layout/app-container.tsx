4eaa64f979479d34c59c77e8a993806fbc02434b
phirom-02 <phiromkhim02@gmail.com>
2025-04-01 16:47:34 +0700

    feat: create app-container ui component

---
+++ src/components/layout/app-container.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AppContainer({ children }: Readonly<Props>) {
+  return <div className="h-screen relative flex flex-col">{children}</div>;
+}
