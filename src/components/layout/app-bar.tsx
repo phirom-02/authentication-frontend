3794316e7702ee558144fef2eb7082ea8fe74d08
phirom-02 <phiromkhim02@gmail.com>
2025-04-02 23:01:18 +0700

    refactor: update theme

--- src/components/layout/app-bar.tsx
+++ src/components/layout/app-bar.tsx
@@ -1,6 +1,6 @@
 export default function AppBar() {
   return (
-    <div className="p-2 bg-blue-500 text-blue-50 sticky top-0">
+    <div className="p-2 bg-primary font-bold text-blue-50 sticky top-0">
       <h1>App Bar</h1>
     </div>
   );
